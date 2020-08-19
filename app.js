b3608f0b29983e1740eba4cb09cbb960bd433f0d
Harley Garcia <hagarcia99@outlook.com>
2020-08-18 21:41:16 -0500

    Newletter api done

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const { urlencoded } = require("body-parser");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+// app.use(config.apiKey);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/html/signup.html");
@@ -13,11 +15,40 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    var data = {
+        members: [
+            {
+              email_address: email,
+              status: "subscribed",
+              merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+              }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://usX.api.mailchimp.com/3.0/lists/*insert your list ID here";
+    
+    const options = {
+      method: "POST",
+      auth: "Name1:*insert your API key here*"
+    }
+    
+    const request = https.request(url, options, function(response) {
+      response.on("data", function(data) {
+        console.log(JSON.parse(data));
+    });
+    
+    });
+    
+    request.write(jsonData);
+    request.end();
 })
 
 app.listen(3000, function() {
